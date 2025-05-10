8b6c0d32fc8e3b6780b2fea0adcfbfad7437e3ec
devansh arora <106925511+dev0919@users.noreply.github.com>
2025-05-10 22:25:32 +0530

    Update and rename data_segmentation.sql to 10_data_segmentation.sql

--- scripts/10_data_segmentation.sql
+++ scripts/10_data_segmentation.sql
@@ -1,11 +1,17 @@
+/*
+===============================================================================
 Data Segmentation Analysis
 ===============================================================================
 Purpose:
     - To group data into meaningful categories for targeted insights.
     - For customer segmentation, product categorization, or regional analysis.
 
+SQL Functions Used:
+    - CASE: Defines custom segmentation logic.
+    - GROUP BY: Groups data into segments.
 ===============================================================================
 */
+
 
 /*Segment products into cost ranges and 
 count how many products fall into each segment*/
